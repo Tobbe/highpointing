5143cf9c50f5454fc4a31c089238609af8abd86f
Tobbe Lundberg <tobbe@tlundberg.com>
2014-03-02 16:19:23 +0100

    Alternating route colors for different days

--- itinerary.js
+++ itinerary.js
@@ -1,6 +1,9 @@
 function Itinerary() {
     this.itinerary = [];
     this.onLoadCompleteFunction = function() {};
+    Object.defineProperty(this, "length", {
+        get: function() { return this.itinerary.length; },
+    });
 }
 
 Itinerary.prototype.loadJSON = function(jsonPath) {
@@ -39,8 +42,14 @@ Itinerary.prototype.onLoadComplete = function(func) {
     this.onLoadCompleteFunction = func;
 };
 
+/**
+ * Special forEach implementation where you can stop the iteration by
+ * returning false from the callback function
+ */
 Itinerary.prototype.forEach = function(fn) {
-    this.itinerary.forEach(function (item) {
-        fn(item);
-    });
+    for (var i = 0; i < this.itinerary.length; ++i) {
+        if (fn(this.itinerary[i], i) === false) {
+            break;
+        }
+    }
 };
